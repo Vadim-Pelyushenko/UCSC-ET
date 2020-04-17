20832fb964b06aabad8ed1dce96935f1b5e05eee
Ismael Chavez III <44008758+ischavez@users.noreply.github.com>
2020-04-17 15:41:54 -0700

    Started trying some requests stuff

--- request.py
+++ request.py
@@ -18,14 +18,25 @@ subjectListValues = [i.attrs.get('value') for i in subjects.findAll('option')]
 
 #Quick check to make sure the lengths are the same
 assert len(subjectListText) == len(subjectListValues)
-print (len(subjectListText))
+# print (len(subjectListText))
 
 #Print the text shown to the user, and the value of that option
-for x in range(len(subjectListText)):
-    print(subjectListText[x])
-    print(subjectListValues[x])
+# for x in range(len(subjectListText)):
+#     print(subjectListText[x])
+#     print(subjectListValues[x])
 
+#NOTE: These are the computer science and engineering elements
+# print(subjectListText[21])
+# print(subjectListValues[21])
 
-# for subjectText in subjectListText:
-#     print(subjectText)
-    # print(subject.text)
+info = {
+    'binds[:term]': '2202',
+    'binds[:reg_status]': 'all',
+    'binds[:subject]': 'CSE'
+        }
+
+# https://requests.readthedocs.io/en/master/api/#requests.Response
+# The type of object that is returned
+resp = requests.post(URL, data=info)
+
+print(resp.text)
